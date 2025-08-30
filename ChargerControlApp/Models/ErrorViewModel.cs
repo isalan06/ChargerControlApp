ef4edf2a2f2dda7e7729ca74f6f2124882138a66
Strawberry Huang <alan.huang6@msa.hinet.net>
2025-08-30 10:54:04 +0800

    加入專案檔案。

---
+++ ChargerControlApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChargerControlApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
